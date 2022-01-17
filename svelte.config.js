ad60794d6cadb41ea71a03c460b75ba49ee102b8
Niklas Jeske <31368200+Niktendo@users.noreply.github.com>
2022-01-17 08:44:56 +0100

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -43,17 +43,4 @@ const config = {
 		}
 	}
 };
-import adapter from '@sveltejs/adapter-static';
-
-export default {
-	kit: {
-		adapter: adapter({
-			// default options are shown
-			pages: 'build',
-			assets: 'build',
-			fallback: null,
-			precompress: false
-		})
-	}
-};
 export default config;
