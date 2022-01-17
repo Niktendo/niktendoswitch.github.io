38b20b97fc33c8965797e8618292c3b31720da76
Niklas Jeske <31368200+Niktendo@users.noreply.github.com>
2022-01-17 08:28:29 +0100

    committee

--- svelte.config.js
+++ svelte.config.js
@@ -43,5 +43,17 @@ const config = {
 		}
 	}
 };
+import adapter from '@sveltejs/adapter-static';
 
+export default {
+	kit: {
+		adapter: adapter({
+			// default options are shown
+			pages: 'build',
+			assets: 'build',
+			fallback: null,
+			precompress: false
+		})
+	}
+};
 export default config;
